376b40c5b5a2e936e9d0ee831afd00d48fcacdf3
pavel <novoid86@yandex.ru>
2019-10-26 18:48:20 +0300

    TDD

---
+++ TDD/factorize.py
@@ -0,0 +1,7 @@
+def factorize(x):
+    """
+    Faztorize positive integer and returns its factors.
+    :type x: int,>=0
+    :rtype: tuple[N],N>0
+    """
+    pass
---
+++ test.py
@@ -0,0 +1,16 @@
+import click
+
+
+@click.command()
+@click.argument('test_names', nargs=-1)
+def test(test_names):
+    import unittest
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('TDD')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    test()
